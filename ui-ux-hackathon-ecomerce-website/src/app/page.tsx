0e373035a38b8dd8811ae8d1c7f0b3d1f3ec1049
Your Name <muhammad04rahim03s@gmail.com>
2025-07-28 17:13:46 +0500

    fixed es lint error

--- ui-ux-hackathon-ecomerce-website/src/app/page.tsx
+++ ui-ux-hackathon-ecomerce-website/src/app/page.tsx
@@ -2,23 +2,18 @@ import Blog from "@/components/home/blog";
 import Hero from "@/components/home/hero";
 import Insta from "@/components/home/insta";
 import MoreProducts from "@/components/home/moreProducts";
-import Navbar1 from "@/components/home/navbar1";
 import NewArrivals from "@/components/home/newArrivals";
 import Products from "@/components/home/products";
-
-
-
-
 
 export default function Home() {
   return (
     <div>
-        <Hero />
-       <MoreProducts/>
-        <Products/>
-       <NewArrivals/>
-       <Blog/>
-      <Insta/> 
+      <Hero />
+      <MoreProducts />
+      <Products />
+      <NewArrivals />
+      <Blog />
+      <Insta />
     </div>
   );
 }
